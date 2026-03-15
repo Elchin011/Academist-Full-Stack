ab0c00b7d554ad3575236fc9c65b9d92803f0d3d
Elchin Yusifov <elchinshy@code.edu.az>
2026-03-16 01:26:42 +0400

    feat: add storybook setup with button, header and page stories

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -9,10 +12,8 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript",
-    "@typescript-eslint/no-explicit-any"
-  ),
-];
+const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript",
+  "@typescript-eslint/no-explicit-any"
+), ...storybook.configs["flat/recommended"]];
 
 export default eslintConfig;
